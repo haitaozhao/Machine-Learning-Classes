7b0add2e169b5aa0cd1d20014398584e167a6ba8
Haitao Zhao <haitaozhao@ecust.edu.cn>
2021-04-26 14:57:33 +0800

    add parzen window estimation

--- 3 K Nearest Neighbors/myKNN.m
+++ 3 K Nearest Neighbors/myKNN.m
@@ -8,8 +8,8 @@ Len = length(y);
 % compute the label of each points in  axis([0,12,0,8]) by KNN
 % draw each points: Red for y = -1 and Blue for y = 1
 % draw the decision region of point (4,4,2) with color 'm'
-for i = 0:0.05:12
-    for j = 0:0.05:8
+for i = 0:0.1:12
+    for j = 0:0.1:8
         temp = [i;j];
         temp2 = temp*ones(1,Len) - x;
         temp_sqr = sqrt(sum(temp2.^2,1));
@@ -35,8 +35,8 @@ Len = length(y);
 % compute the label of each points in  axis([0,12,0,8]) by KNN
 % draw each points: Red for y = -1 and Blue for y = 1
 % draw the decision region of point (4,4,2) with color 'm'
-for i = 0:0.05:12
-    for j = 0:0.05:8
+for i = 0:0.1:12
+    for j = 0:0.1:8
         temp = [i;j];
         temp2 = temp*ones(1,Len) - x;
         temp_sqr = sqrt(sum(temp2.^2,1));
